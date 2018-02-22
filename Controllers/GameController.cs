3eeea6f4a5053dc94742b4128170df672b8481b3
tB1ack <31832634+tB1ack@users.noreply.github.com>
2018-02-21 20:20:15 -0800

    Add files via upload

---
+++ Controllers/GameController.cs
@@ -0,0 +1,49 @@
+﻿using FinalKnockoutJS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FinalKnockoutJS.Controllers
+{
+    public class GameController : ApiController
+    {
+        // GET: api/Game
+        public IEnumerable<Game> Get()
+        {
+            var game = GameRepository.GetGames();
+            return game.ToList();
+        }
+
+        // GET: api/Game/5
+        public Game Get(int id)
+        {
+            return GameRepository.GetGames().Where((g) => g.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/Game
+        public HttpResponseMessage Post(Game value)
+        {
+            GameRepository.InsertGame(value);
+
+            var response = Request.CreateResponse<Game>(HttpStatusCode.Created, value);
+
+            string uri = Url.Link("DefaultApi", new { id = value.Id });
+            response.Headers.Location = new Uri(uri);
+
+            return response;
+        }
+
+        // PUT: api/Game/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Game/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Scripts/GameViewModel.js
@@ -0,0 +1,40 @@
+﻿/// <reference path="jquery-3.3.1.min.js" />
+/// <reference path="knockout-3.4.2.js" />
+
+function game(id, name, genre, console) {
+    var self = this;
+
+    self.Id = id;
+    self.Name = name;
+    self.Genre = genre;
+    self.Console = console;
+
+    self.addGame = function () {
+        $.ajax({
+            url: "/api/Game/",
+            type: 'post',
+            data: ko.toJSON(this),
+            contentType: 'application/json',
+            success: function (result) {
+            }
+        });
+    };
+}
+
+function gameVM() {
+    var self = this;
+    self.games = ko.observableArray([]);
+    self.getGames = function () {
+        self.games.removeAll();
+        $.getJSON("/api/Game/", function (data) {
+            $.each(data, function (key, val) {
+                self.games.push(new game(val.Id, val.Name, val.Genre, val.Console));
+            });
+        });
+    };
+}
+
+$(document).ready(function () {
+    ko.applyBindings(new gameVM(), document.getElementById('displayNode'));
+    ko.applyBindings(new game(), document.getElementById('createNode'));
+});
